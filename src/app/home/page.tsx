51bcbdfd078a1683553428b1e8e33152a3589acd
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-08-09 13:09:26 +0900

    コメントを追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -11,6 +11,7 @@ const Home: React.FC = () => {
       <Button primary label="保存" />
       <br />
 
+      {/* tailwind cssの設定が反映されているか確認用 */}
       <p className="text-3xl text-red-500 font-bold">Hello Tailwind</p>
     </div>
   );
