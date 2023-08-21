9cee768620727067e4750e0a202fb41a0408f012
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-08-21 14:27:31 +0900

    tailwind.config.js add semantic colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/** @type {import('tailwindcss').Config} */
+
 const white = {
   1000: '#FFFFFF',
 };
@@ -94,15 +96,65 @@ const primitiveColors = {
 const semanticColors = {
   text: {
     light: {
-      body: primitiveColors.wood[200],
+      body: primitiveColors.sumi[900],
+      description: primitiveColors.sumi[700],
+      placeHolder: primitiveColors.sumi[600],
+      onFill: primitiveColors.white[1000],
+      link: primitiveColors.sea[800],
+      hover: primitiveColors.sea[900],
+      active: primitiveColors.sea[900],
+      visited: primitiveColors.sea[900],
+      alert: primitiveColors.sun[800],
+      disabled: primitiveColors.sumi[500],
     },
   },
-  red: {
-    900: '#ff0000',
+  icon: {
+    light: {
+      label: primitiveColors.sumi[900],
+      active: primitiveColors.sea[800],
+      alert: primitiveColors.sun[800],
+      disabled: primitiveColors.sumi[500],
+    },
+  },
+  button: {
+    light: {
+      normal: primitiveColors.sea[800],
+      hover: primitiveColors.sea[800], // Todo:透過の設定が必要
+      active: primitiveColors.sea[800], // Todo:透過の設定が必要
+      disabledBg: primitiveColors.sumi[500],
+    },
+  },
+  background: {
+    light: {
+      primary: primitiveColors.white[1000],
+      secondary: primitiveColors.sumi[100],
+      tertiary: primitiveColors.sumi[50],
+    },
+  },
+  border: {
+    // Todo:全体的にborderの太さについて指定が必要
+    light: {
+      field: primitiveColors.sumi[900],
+      divider: primitiveColors.sumi[300],
+      focused: primitiveColors.wood[600],
+      selected: primitiveColors.sea[800],
+      alert: primitiveColors.sun[800],
+      disabled: primitiveColors.sumi[300],
+    },
+  },
+  status: {
+    light: {
+      success: primitiveColors.forest[600],
+      alert: primitiveColors.sun[800],
+      warning: primitiveColors.wood[800],
+    },
+  },
+  chart: {
+    primary: primitiveColors.sea[800],
+    secondary: primitiveColors.sea[400],
   },
 };
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/app/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -111,12 +163,6 @@ module.exports = {
     },
     extend: {
       colors: semanticColors,
-      // colors: {
-      //   semanticColors,
-      //   // red: {
-      //   //   900: '#ff0000',
-      //   // },
-      // },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
